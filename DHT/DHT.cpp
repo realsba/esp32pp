a4ba62e9109ee6e7019a89730254ccf5adcae154
Bohdan Sadovyak <bohdan.sadovyak@gmail.com>
2022-07-31 11:09:38 +0300

    Added implementation of DHT11 and DHT22 sensors

---
+++ DHT/DHT.cpp
@@ -0,0 +1,228 @@
+#include "DHT.hpp"
+
+#include <driver/gpio.h>
+#include <driver/rmt_rx.h>
+
+#include <esp_log.h>
+
+#include <limits>
+
+static const char* TAG = "DHT";
+
+uint DHT::s_nextId {0};
+
+DHT::~DHT()
+{
+  if (m_channel) {
+    rmt_del_channel(m_channel);
+  }
+}
+
+void DHT::setup(gpio_num_t gpio, uint32_t clockResolution)
+{
+  m_pin = gpio;
+
+  ESP_LOGI(TAG, "Create RMT RX channel");
+  rmt_rx_channel_config_t channelConfig = {
+    .gpio_num = gpio,
+    .clk_src = RMT_CLK_SRC_DEFAULT,
+    .resolution_hz = clockResolution,
+    .mem_block_symbols = 64,
+    .flags = {
+      .invert_in = false,
+      .with_dma = false,
+      .io_loop_back = false
+    }
+  };
+  ESP_ERROR_CHECK(rmt_new_rx_channel(&channelConfig, &m_channel));
+
+  ESP_LOGI(TAG, "Register RX done callback");
+  rmt_rx_event_callbacks_t cbs = {
+    .on_recv_done = rx_done_callback,
+  };
+
+  ESP_LOGI(TAG, "task addr %p", m_task.getNativeHandle());
+  ESP_ERROR_CHECK(rmt_rx_register_event_callbacks(m_channel, &cbs, m_task.getNativeHandle()));
+
+  gpio_set_direction(m_pin, GPIO_MODE_OUTPUT);
+  gpio_set_level(m_pin, 1);
+}
+
+void DHT::read(Handler&& handler)
+{
+  const static rmt_receive_config_t config = {
+    .signal_range_min_ns = 5000,
+    .signal_range_max_ns = 120000
+  };
+
+  m_handler = std::move(handler);
+  m_status = 0;
+
+  gpio_set_direction(m_pin, GPIO_MODE_OUTPUT);
+  gpio_set_level(m_pin, 0);
+  esp_rom_delay_us(m_startSignalDuration);
+  rmt_enable(m_channel);
+  ESP_ERROR_CHECK(rmt_receive(m_channel, m_symbols, sizeof(m_symbols), &config));
+  gpio_set_direction(m_pin, GPIO_MODE_INPUT);
+}
+
+bool IRAM_ATTR DHT::rx_done_callback(rmt_channel_handle_t channel, const rmt_rx_done_event_data_t* eventData, void* context)
+{
+  BaseType_t highTaskWakeup = pdFALSE;
+  auto taskToNotify = static_cast<TaskHandle_t>(context);
+  xTaskNotifyFromISR(taskToNotify, reinterpret_cast<uint32_t>(eventData), eSetValueWithOverwrite, &highTaskWakeup);
+  return highTaskWakeup == pdTRUE;
+}
+
+void DHT::readSensor()
+{
+  rmt_rx_done_event_data_t* eventData {nullptr};
+
+  if (xTaskNotifyWait(0x00, ULONG_MAX, reinterpret_cast<uint32_t*>(&eventData), portMAX_DELAY) == pdTRUE) {
+    ESP_LOGI(TAG, "Decode a response %p %d", eventData->received_symbols, eventData->num_symbols);
+
+    decode(eventData->received_symbols, eventData->num_symbols);
+    ESP_LOGI(TAG, "status %d", (int) m_status);
+
+    rmt_disable(m_channel);
+    gpio_set_direction(m_pin, GPIO_MODE_OUTPUT);
+    gpio_set_level(m_pin, 1);
+
+    if (m_status == 0) {
+      if (m_handler) {
+        m_handler(std::error_code {}, humidity(), temperature());
+      }
+    }
+  }
+}
+
+void DHT::decode(rmt_symbol_word_t* data, size_t numItems)
+{
+  m_data[0] = 0;
+  m_data[1] = 0;
+  m_data[2] = 0;
+  m_data[3] = 0;
+  m_data[4] = 0;
+  m_status = 0;
+
+  if (numItems < 42) {
+    m_status = 1;
+    for (int i = 0; i < numItems; ++i) {
+      printf("%d: %d %d, %d %d\n", i, (int)data[i].level0, (int)data[i].duration0, (int)data[i].level1, (int)data[i].duration1);
+    }
+    return;
+  }
+
+  auto* ptr = reinterpret_cast<rmt_symbol_word_t*>(reinterpret_cast<uint8_t*>(data) + 2);
+  --numItems;
+
+  for (int i = 0; i < numItems; ++i) {
+    printf("%d: %d %d, %d %d\n", i, (int)ptr[i].level0, (int)ptr[i].duration0, (int)ptr[i].level1, (int)ptr[i].duration1);
+  }
+
+  uint32_t duration = ptr[0].duration0 + ptr[0].duration1;
+  if (duration < 140 || duration > 180) {
+    ESP_LOGE(TAG, "Bad 'Response signal'");
+    m_status = 2;
+    return;
+  }
+
+  for (int i = 1; i < numItems - 1; ++i) {
+    if (ptr[i].level0 != 0 || ptr[i].level1 != 1) {
+      ESP_LOGE(TAG, "Incorrect level");
+      m_status = 3;
+      return; // error
+    }
+
+    duration = ptr[i].duration0;
+    if (duration < 48 || duration > 55) {
+      ESP_LOGE(TAG, "Incorrect duration0");
+      m_status = 4;
+      return; // error
+    }
+    m_data[(i - 1) / 8] <<= 1;
+    duration = ptr[i].duration1;
+    if (duration >= 68 && duration <= 75) {
+      m_data[(i - 1) / 8] |= 1;
+    } else if (duration < 22 || duration > 30) {
+      ESP_LOGE(TAG, "Incorrect duration1");
+      m_status = 5;
+      return; // error
+    }
+  }
+
+  if (m_data[4] == ((m_data[0] + m_data[1] + m_data[2] + m_data[3]) & 0xFF)) {
+    m_status = 0;
+  } else {
+    m_status = 6;  // checksum error
+  }
+}
+
+DHT11::DHT11()
+{
+  m_startSignalDuration = 30000;
+}
+
+float DHT11::temperature()
+{
+  if (m_status != 0) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  return static_cast<float>(m_data[2]);
+}
+
+float DHT11::humidity()
+{
+  if (m_status != 0) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  return static_cast<float>(m_data[0]);
+}
+
+float DHT22::temperature()
+{
+  if (m_status != 0) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  auto temp = static_cast<float>((((m_data[2] & 0x7F) << 8) | m_data[3]) * 0.1);
+  if (m_data[2] & 0x80) {
+    temp = -temp;
+  }
+  return temp;
+}
+
+float DHT22::humidity()
+{
+  if (m_status != 0) {
+    return std::numeric_limits<float>::quiet_NaN();
+  }
+  return static_cast<float>(((m_data[0] << 8) | m_data[1]) * 0.1);
+}
+
+namespace error {
+namespace detail {
+
+const std::error_category& DhtCategory::getInstance()
+{
+  static const DhtCategory instance;
+  return instance;
+}
+
+const char* DhtCategory::name() const noexcept
+{
+  return "DHT";
+}
+
+std::string DhtCategory::message(int ev) const
+{
+  using category = error::DhtCategory;
+
+  switch (static_cast<category>(ev)) {
+    case category::Failed: return "failed";
+  }
+
+  return "Undefined error";
+}
+
+} // namespace detail
+} // namespace error
---
+++ DHT/DHT.hpp
@@ -0,0 +1,103 @@
+#ifndef ESP32PP_DHT_HPP
+#define ESP32PP_DHT_HPP
+
+#include "Task.hpp"
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+#include <driver/rmt_types.h>
+#include <driver/rmt_rx.h>
+#include <driver/gpio.h>
+
+#include <functional>
+#include <system_error>
+
+class DHT
+{
+public:
+  using Handler = std::function<void(std::error_code ec, float humidity, float temperature)>;
+
+  virtual ~DHT();
+
+  void setup(gpio_num_t gpio, uint32_t clockResolution);
+  void read(Handler&& handler);
+
+private:
+  static bool rx_done_callback(rmt_channel_handle_t channel, const rmt_rx_done_event_data_t* eventData, void* context);
+
+  void readSensor();
+  void decode(rmt_symbol_word_t* data, size_t numItems);
+
+  virtual float temperature() = 0;
+  virtual float humidity() = 0;
+
+protected:
+  uint16_t m_startSignalDuration {1000};
+  uint8_t m_status {0};
+  uint8_t m_data[5] {};
+
+private:
+  static uint s_nextId;
+
+  uint m_id = s_nextId;
+  rmt_symbol_word_t m_symbols[64];
+  Task m_task {[this] { readSensor(); }, "DHT:" + std::to_string(m_id)};
+  Handler m_handler;
+  rmt_channel_handle_t m_channel {nullptr};
+  gpio_num_t m_pin {GPIO_NUM_0};
+};
+
+class DHT11 : public DHT
+{
+public:
+  explicit DHT11();
+
+private:
+  float temperature() override;
+  float humidity() override;
+};
+
+class DHT22 : public DHT
+{
+  float temperature() override;
+  float humidity() override;
+};
+
+namespace error {
+
+enum class DhtCategory
+{
+  Failed = 1,
+};
+
+namespace detail {
+
+class DhtCategory : public std::error_category
+{
+public:
+  DhtCategory() = default;
+
+  static const std::error_category& getInstance();
+
+  const char* name() const noexcept override;
+  std::string message(int ev) const override;
+};
+
+} // namespace detail
+
+inline std::error_code make_error_code(DhtCategory e)
+{
+  return {static_cast<int>(e), detail::DhtCategory::getInstance()};
+}
+
+} // namespace error
+
+namespace std {
+
+template<>
+struct is_error_code_enum<error::DhtCategory> : public true_type {};
+
+} // namespace std
+
+#endif // ESP32PP_DHT_HPP
