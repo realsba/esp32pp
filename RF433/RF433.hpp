1195b6b0bdd595ae0d216dffa936ce6111755ec8
Bohdan Sadovyak <bohdan.sadovyak@gmail.com>
2023-12-02 15:59:21 +0200

    RF433: Base implementation

---
+++ RF433/RF433.hpp
@@ -0,0 +1,42 @@
+#ifndef ESP32PP_RF433_HPP
+#define ESP32PP_RF433_HPP
+
+#include <chrono>
+
+#include <driver/rmt_encoder.h>
+
+class RF433
+{
+public:
+  ~RF433();
+
+  void setup(uint8_t gpio);
+  bool send(const void* data, size_t dataSize);
+
+private:
+  static size_t encode(
+    rmt_encoder_t* encoder,
+    rmt_channel_handle_t channel,
+    const void* data, size_t dataSize,
+    rmt_encode_state_t* retState
+  );
+  static esp_err_t reset(rmt_encoder_t* encoder);
+
+  enum class State
+  {
+    SendData,
+    SendResetCode
+  };
+
+  size_t encode(rmt_channel_handle_t channel, const void* data, size_t dataSize, rmt_encode_state_t* retState);
+  esp_err_t reset();
+
+  rmt_encoder_t m_encoder {.encode = encode, .reset = reset, .del = nullptr};
+  rmt_channel_handle_t m_channel {nullptr};
+  rmt_encoder_handle_t m_bytesEncoder {nullptr};
+  rmt_encoder_handle_t m_copyEncoder {nullptr};
+  rmt_symbol_word_t m_resetCode {};
+  State m_state {State::SendData};
+};
+
+#endif // ESP32PP_RF433_HPP
