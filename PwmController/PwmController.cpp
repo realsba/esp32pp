e435c30733cfcf643e598cd4ce5724d86e96fbb4
Bohdan Sadovyak <bohdan.sadovyak@gmail.com>
2024-08-17 13:39:38 +0300

    Base implementation of PWM

---
+++ PwmController/PwmController.cpp
@@ -0,0 +1,41 @@
+#include "PwmController.hpp"
+
+PwmController::PwmController(gpio_num_t pin, uint32_t frequency, uint32_t dutyCycle)
+  : m_pin(pin), m_frequency(frequency), m_dutyCycle(dutyCycle)
+{
+  init();
+}
+
+void PwmController::setDutyCycle(uint32_t value)
+{
+  m_dutyCycle = value;
+  ledc_set_duty(LEDC_HIGH_SPEED_MODE, m_ledcChannel.channel, m_dutyCycle);
+  ledc_update_duty(LEDC_HIGH_SPEED_MODE, m_ledcChannel.channel);
+}
+
+void PwmController::init()
+{
+  const auto timerConfig = ledc_timer_config_t {
+    .speed_mode       = LEDC_HIGH_SPEED_MODE,
+    .duty_resolution  = LEDC_TIMER_10_BIT,
+    .timer_num        = LEDC_TIMER_0,
+    .freq_hz          = m_frequency,
+    .clk_cfg          = LEDC_AUTO_CLK,
+    .deconfigure      = false
+  };
+  ledc_timer_config(&timerConfig);
+
+  m_ledcChannel = {
+    .gpio_num       = m_pin,
+    .speed_mode     = LEDC_HIGH_SPEED_MODE,
+    .channel        = LEDC_CHANNEL_0,
+    .intr_type      = LEDC_INTR_DISABLE,
+    .timer_sel      = LEDC_TIMER_0,
+    .duty           = m_dutyCycle,
+    .hpoint         = 0,
+    .flags          = {.output_invert = false}
+  };
+  ledc_channel_config(&m_ledcChannel);
+  ledc_set_duty(LEDC_HIGH_SPEED_MODE, m_ledcChannel.channel, m_dutyCycle);
+  ledc_update_duty(LEDC_HIGH_SPEED_MODE, m_ledcChannel.channel);
+}
---
+++ PwmController/PwmController.hpp
@@ -0,0 +1,21 @@
+#ifndef PWMCONTROLLER_HPP
+#define PWMCONTROLLER_HPP
+
+#include <driver/ledc.h>
+
+class PwmController {
+public:
+  PwmController(gpio_num_t pin, uint32_t frequency, uint32_t dutyCycle);
+
+  void setDutyCycle(uint32_t value);
+
+private:
+  void init();
+
+  gpio_num_t                    m_pin;
+  uint32_t                      m_frequency;
+  uint8_t                       m_dutyCycle;
+  ledc_channel_config_t         m_ledcChannel {};
+};
+
+#endif // PWMCONTROLLER_HPP
